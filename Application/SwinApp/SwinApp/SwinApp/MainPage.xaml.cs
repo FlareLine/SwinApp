01bb4ba0177a4549f68a98ec736328583c99b2c2
pielegacy <alex_billson@outlook.com>
2017-04-07 23:26:35 +1000

    Working on Menu system?

--- Application/SwinApp/SwinApp/SwinApp/MainPage.xaml.cs
+++ Application/SwinApp/SwinApp/SwinApp/MainPage.xaml.cs
@@ -14,11 +14,27 @@ namespace SwinApp
 		{
 			InitializeComponent();
             ListDashboard.ItemsSource = User.DashBoardItems;
+            ListMenu.ItemsSource = new List<MenuItem>
+            {
+                new MenuItem("Timetable", "See your classes"),
+                new MenuItem("Campus", "Find your way around"),
+                new MenuItem("Transport", "Get home easily")
+            };
 		}
         protected override void OnAppearing()
         {
             NavigationPage.SetHasNavigationBar(this, false);
             base.OnAppearing();
+        }
+    }
+    public class MenuItem
+    {
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public MenuItem(string t, string d)
+        {
+            Title = t;
+            Desc = d;
         }
     }
 }
