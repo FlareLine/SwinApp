59fe8f4a5aa9a1ea105916494c32179bbdda32d5
pielegacy <alex_billson@outlook.com>
2017-04-16 10:20:26 +1000

    Removed UI thing

--- Application/SwinApp/SwinApp/SwinApp/Components/UI/DialogBBAnnouncement.xaml.cs
+++ Application/SwinApp/SwinApp/SwinApp/Components/UI/DialogBBAnnouncement.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace SwinApp.Components.UI
+namespace SwinApp.Components
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class DialogBBAnnouncement : ContentPage
