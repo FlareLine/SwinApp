64d48cb191a1f4ba867b1e83cfdaa35ea6ba60bd
FlareLine <101133651@swin.edu.au>
2017-04-18 22:07:23 +1000

    Fixing merging issues, and resetting git environment. This is now the proper branch for transport related development

--- Application/SwinApp/SwinApp/SwinApp/MainPage.xaml.cs
+++ Application/SwinApp/SwinApp/SwinApp/MainPage.xaml.cs
@@ -9,60 +9,60 @@ using Xamarin.Forms.Xaml;
 
 namespace SwinApp
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : TabbedPage
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : TabbedPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
-            ListDashboard.ItemsSource = User.DashBoardItems;
-            ListMenu.ItemsSource = new List<MenuItem>
-            {
-                new MenuItem("Timetable", "See your classes"),
-                new MenuItem("Campus", "Find your way around"),
-                new MenuItem("Transport", "Get home easily"),
-                new MenuItem("Announcements", "Keep in the loop with all going on at Uni")
-                {
-                    Page = new AnnouncementPage()
-                }
-            };
-            ListMenu.ItemTapped += MenuSelection;
+			ListDashboard.ItemsSource = User.DashBoardItems;
+			ListMenu.ItemsSource = new List<MenuItem>
+			{
+				new MenuItem("Timetable", "See your classes"),
+				new MenuItem("Campus", "Find your way around"),
+				new MenuItem("Transport", "Get home easily"),
+				new MenuItem("Announcements", "Keep in the loop with all going on at Uni")
+				{
+					Page = new AnnouncementPage()
+				}
+			};
+			ListMenu.ItemTapped += MenuSelection;
 		}
 
-        private async void MenuSelection(object sender, ItemTappedEventArgs e)
-        {
-            var menuItem = ListMenu.SelectedItem as MenuItem;
-            if (menuItem.Page != null)
-                await Navigation.PushAsync(menuItem.Page);
-        }
+		private async void MenuSelection(object sender, ItemTappedEventArgs e)
+		{
+			var menuItem = ListMenu.SelectedItem as MenuItem;
+			if (menuItem.Page != null)
+				await Navigation.PushAsync(menuItem.Page);
+		}
 
-        protected override async void OnAppearing()
-        {
-            NavigationPage.SetHasNavigationBar(this, false);
-            base.OnAppearing();
-            try
-            {
-                await User.LoadUserData();
-            }
-            catch (Exception e)
-            {
-                User.DashBoardItems.Add(new TextContentDashCard("An Error Occurred", $"Details: {e.Message}"));
-            }
-        }
+		protected override async void OnAppearing()
+		{
+			NavigationPage.SetHasNavigationBar(this, false);
+			base.OnAppearing();
+			try
+			{
+				await User.LoadUserData();
+			}
+			catch (Exception e)
+			{
+				User.DashBoardItems.Add(new TextContentDashCard("An Error Occurred", $"Details: {e.Message}"));
+			}
+		}
 
-        private async void ShowContextMenu(object sender, EventArgs e) => await DisplayActionSheet("Add New...", "Close", "", new string[] { "Reminder" });
+		private async void ShowContextMenu(object sender, EventArgs e) => await DisplayActionSheet("Add New...", "Close", "", new string[] { "Reminder" });
 
-        private void AssertPlusVisibility(object sender, ScrolledEventArgs e) => ButtonAndroidPlus.IsVisible = ScrollFeed.ScrollY > 0 ? false : true;
-    }
-    public class MenuItem
-    {
-        public string Title { get; set; }
-        public string Desc { get; set; }
-        public Page Page { get; set; }
-        public MenuItem(string t, string d)
-        {
-            Title = t;
-            Desc = d;
-        }
-    }
+		private void AssertPlusVisibility(object sender, ScrolledEventArgs e) => ButtonAndroidPlus.IsVisible = ScrollFeed.ScrollY > 0 ? false : true;
+	}
+	public class MenuItem
+	{
+		public string Title { get; set; }
+		public string Desc { get; set; }
+		public Page Page { get; set; }
+		public MenuItem(string t, string d)
+		{
+			Title = t;
+			Desc = d;
+		}
+	}
 }
