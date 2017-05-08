d885efd69ceee08967f058889cd97949ccd3a42a
ChoiceKarpz <campbell_graham@outlook.com>
2017-05-08 12:48:27 +1000

    Menu has icons

--- Application/SwinApp/SwinApp/SwinApp/LinksPage.xaml.cs
+++ Application/SwinApp/SwinApp/SwinApp/LinksPage.xaml.cs
@@ -18,15 +18,15 @@ namespace SwinApp
 
 		public LinksPage ()
 		{
-            Links.Add(new CardExternalLink("Swinburne", "The site of our favourite university <3", new Uri("https://www.swinburne.edu.au/")));
-            Links.Add(new CardExternalLink("Blackboard", "For all your unit needs", new Uri("http://ilearn.swin.edu.au/")));
-            Links.Add(new CardExternalLink("Allocate", "For when that regular timetable just isn't cutting it", new Uri("https://allocate.swin.edu.au/aplus-2017/timetable/#subjects")));
-            Links.Add(new CardExternalLink("Study Spaces", "Find a spot where there's actually a powerpoint for your laptop", new Uri("http://m.swinburne.edu.au/study-spaces/#/")));
-            Links.Add(new CardExternalLink("News", "Keep up to date with the latest and greatest", new Uri("http://www.swinburne.edu.au/news/")));
-            Links.Add(new CardExternalLink("Staff", "Get the email address of your favourite tutor", new Uri("https://www.swin.edu.au/directory/")));
-            Links.Add(new CardExternalLink("Llibrary", "Get the books you need for that last minute assingment cram", new Uri("http://www.swinburne.edu.au/library/")));
-            Links.Add(new CardExternalLink("Safe@Swin", "Everyone deserves a right to be comfortable at university", new Uri("https://play.google.com/store/apps/details?id=com.cutcom.apparmor.swin&hl=en")));
-            Links.Add(new CardExternalLink("SSAA", "There to make your time at Swinburne as enjoyable as it can be", new Uri("https://www.swinburne.edu.au/current-students/life/student-organisations/")));
+            Links.Add(new CardExternalLink("Swinburne", "The site of our favourite university <3", new Uri("https://www.swinburne.edu.au/"), "swinburnelogo.png"));
+            Links.Add(new CardExternalLink("Blackboard", "For all your unit needs", new Uri("http://ilearn.swin.edu.au/"), "blackboardlogo.png"));
+            Links.Add(new CardExternalLink("Allocate", "For when that regular timetable just isn't cutting it", new Uri("https://allocate.swin.edu.au/aplus-2017/timetable/#subjects"), "allocatelogo.png"));
+            Links.Add(new CardExternalLink("Study Spaces", "Find a spot where there's actually a powerpoint for your laptop", new Uri("http://m.swinburne.edu.au/study-spaces/#/"), "studyspaceslogo.png"));
+            Links.Add(new CardExternalLink("News", "Keep up to date with the latest and greatest", new Uri("http://www.swinburne.edu.au/news/"), "newslogo.png"));
+            Links.Add(new CardExternalLink("Staff", "Get the email address of your favourite tutor", new Uri("https://www.swin.edu.au/directory/"), "contactslogo.png"));
+            Links.Add(new CardExternalLink("Library", "Get the books you need for that last minute assingment cram", new Uri("http://www.swinburne.edu.au/library/"), "librarylogo.png"));
+            Links.Add(new CardExternalLink("Safe@Swin", "Everyone deserves a right to be comfortable at university", new Uri("https://play.google.com/store/apps/details?id=com.cutcom.apparmor.swin&hl=en"), "safeswinlogo.png"));
+            Links.Add(new CardExternalLink("SSAA", "There to make your time at Swinburne as enjoyable as it can be", new Uri("https://www.swinburne.edu.au/current-students/life/student-organisations/"), "ssaalogo.png"));
             InitializeComponent ();
             ListLinks.ItemsSource = Links;
         }
