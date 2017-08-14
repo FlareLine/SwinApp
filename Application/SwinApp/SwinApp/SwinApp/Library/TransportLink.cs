7550dc887b3e634cea01e148614acced0e87472e
FlareLine <101133651@swin.edu.au>
2017-08-14 20:02:06 +1000

    Transport Page - #1 General library fixes and working on developing tram functionality

--- Application/SwinApp/SwinApp/SwinApp/Library/TransportLink.cs
+++ Application/SwinApp/SwinApp/SwinApp/Library/TransportLink.cs
@@ -5,11 +5,25 @@ using System.Threading.Tasks;
 
 namespace SwinApp.Library
 {
+    /// <summary>
+    /// Provides a layer between the application and the PTV library
+    /// </summary>
     public class TransportLink
     {
-		public async Task<Departure> GetNextDeparture(int r, int d)
+        /// <summary>
+        /// Requests the next departure for the specified route, direction and stop id
+        /// </summary>
+        /// <param name="r">Route id</param>
+        /// <param name="d">Direction id</param>
+        /// <param name="s">Stop id</param>
+        /// <returns>The next departure in a Departure object</returns>
+        /// <remarks>
+        /// Does not differentiate between train and trams as the three parameters in
+        /// combination should be enough to determine the route type.
+        /// </remarks>
+		public async Task<Departure> GetNextDeparture(int r, int d, int s)
 		{
-			return (await PTV.RequestPTVPayloadAsync($"departures/route_type/0/stop/1080/route/{r}?direction_id={d}&max_results=1")).Departures[0];
+            return (await PTV.RequestPTVPayloadAsync($"departures/route_type/0/stop/1080/route/{r}?direction_id={d}&max_results=1")).Departures[0];
 		}
     }
 }
