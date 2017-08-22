5f18499d83b99212493a1fbbea5739e9e40ceb17
FlareLine <101133651@swin.edu.au>
2017-08-22 21:13:52 +1000

    Analytics work - Actions being added so we can log users easily

--- Application/SwinApp/SwinApp/SwinApp/Library/Analytics.cs
+++ Application/SwinApp/SwinApp/SwinApp/Library/Analytics.cs
@@ -2,6 +2,28 @@
 {
     public static class Anaytics
     {
+        public static void LogAction(string action)
+        {
+
+        }
+
+        public static void ConstructLoggedAction()
+        {
+
+        }
+    }
+
+    public class LoggedAction
+    {
+        public ActionType ActionType { get; set; }
+        public string DateTime { get; set; }
+    }
+    public enum ActionType
+    {
+        APP_START = 0,
+        CLICK_LINK_ITEM = 1,
+        CLICK_MENU_ITEM = 2,
+
 
     }
 }
