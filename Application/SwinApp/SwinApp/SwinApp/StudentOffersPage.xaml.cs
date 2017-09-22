615a9afdf20da2dae45f5629fcd00cf66201eaac
FlareLine <101133651@swin.edu.au>
2017-09-22 12:43:03 +1000

    Just some small edits to the Student Offers stuff, also corrected dependency for the CardStudentOffer.xaml.cs file

--- Application/SwinApp/SwinApp/SwinApp/StudentOffersPage.xaml.cs
+++ Application/SwinApp/SwinApp/SwinApp/StudentOffersPage.xaml.cs
@@ -18,7 +18,7 @@ namespace SwinApp
 
         public StudentOffersPage()
         {
-            Offers.Add(new CardStudentOffer("Student Edge", "Student Edge is Australia’s largest member-based organisation of high school and tertiary students, with more than 900,000 members nationally.\nJoin now(for FREE) to access super sweet student discounts on Apple, McDonald’s, Microsoft, Chatime and more of your faves!", new Uri("https://studentedge.org/"), "StudentEdgeLogo.jpg"));
+            Offers.Add(new CardStudentOffer("Student Edge", "Student Edge is Australia’s largest member-based organisation of high school and tertiary students, with more than 900,000 members nationally.\nJoin now (for FREE) to access super sweet student discounts on Apple, McDonald’s, Microsoft, Chatime and more of your faves!", new Uri("https://studentedge.org/"), "StudentEdgeLogo.jpg"));
             //Offers.Add(new CardStudentOffer("Swinburne ELMS", "The Electronic License Management System (ELMS) portal provides an interface for eligible STEM students to obtain license codes and download media for select VMware and Microsoft development tools.", new Uri("https://elms.swin.edu.au/"), "ELMSLogo.png"));
                 /*Text taken directly from Swinburne ELMS portal (on 03/09/17)
                  * Logo currently just a placeholder (03/09/17) */
